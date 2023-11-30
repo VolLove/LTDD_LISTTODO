8ada031d9e32a96fd9128f378a1105a38ae2204e
Nhi <103765155+VolLove@users.noreply.github.com>
2023-11-30 20:58:34 +0700

    Update

--- Project/app/src/main/java/Model/Job.java
+++ Project/app/src/main/java/Model/Job.java
@@ -1,2 +1,93 @@
-package Model;public class Job {
+package Model;
+
+public class Job {
+    String id, title,type_id,decription, date_create,deadline,rank,status,id_user;
+
+    public Job() {
+    }
+
+    public Job(String id, String title, String type_id, String decription, String date_create, String deadline, String rank, String status, String id_user) {
+        this.id = id;
+        this.title = title;
+        this.type_id = type_id;
+        this.decription = decription;
+        this.date_create = date_create;
+        this.deadline = deadline;
+        this.rank = rank;
+        this.status = status;
+        this.id_user = id_user;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(String type_id) {
+        this.type_id = type_id;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    public String getDate_create() {
+        return date_create;
+    }
+
+    public void setDate_create(String date_create) {
+        this.date_create = date_create;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(String id_user) {
+        this.id_user = id_user;
+    }
+
 }
--- Project/app/src/main/java/Model/Type_Job.java
+++ Project/app/src/main/java/Model/Type_Job.java
@@ -1,2 +1,34 @@
-package Model;public class Type_Job {
+package Model;
+
+public class Type_Job {
+    String id, title;
+
+    public Type_Job() {
+    }
+
+    public Type_Job(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title ;
+    }
 }
